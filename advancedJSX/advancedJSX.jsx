4574300fb3436d3346ec0d3fa86014ed6d312dc2
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-09 12:56:31 +0100

    JSX Conditionals: &&

--- advancedJSX/advancedJSX.jsx
+++ advancedJSX/advancedJSX.jsx
@@ -95,3 +95,77 @@ const kitty = (
 );
 
 root.render(kitty);
+
+// If Statements
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+
+if(coinToss() === 'heads') {
+  img = <img src={pics.kitty} />
+} else {
+  img = <img src={pics.doggy} />
+}
+
+root.render(img);
+
+// An another example of an if statement but using ternary operator:
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+
+root.render(img);
+
+// JSX Conditionals: &&
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      // The following line will only render if judgmental is false:
+      { !judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+root.render(favoriteFoods);
