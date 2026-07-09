eb676dccc6fc9f64ba175a24656c1ab04adf3a35
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-09 10:48:22 +0100

    Add initial JSX elements for header, navigation, team roster, and article

---
+++ jsx/jsx.jsx
@@ -0,0 +1,15 @@
+const h1 = <h1>Hello world</h1>;
+
+// JSX Elements and their surroundings
+
+const navBar = <nav>I am a nav bar</nav>;
+
+const myTeam = {
+  center: <li>Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGuard: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billon</li>
+};
+
+const myArticle = <article></article>;
