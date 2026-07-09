c86cf6bbc1d579d8c750d57f989770ef02153d69
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-09 11:35:49 +0100

    self-closing tags

--- advancedJSX/advancedJSX.jsx
+++ advancedJSX/advancedJSX.jsx
@@ -9,5 +9,19 @@ const myDiv = <div className="big">I AM A BIG DIV</div>;
 
 root.render(myDiv);
 
+// Self-closing tags. You have to close self-closing tags in JSX, even if they are empty. For example, <img> and <br> tags must be written as <img /> and <br /> in JSX.
 
+const profile = (
+  <div>
+    <h1>John Smith</h1>
+    <img src="images/john.png" />
+    <article>
+      My name is John Smith.
+      <br />
+      I am a software developer.
+      <br />
+      I specialize in creating React applications.
+    </article>
+  </div>
+);
 
