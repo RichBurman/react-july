679ed0c5394ee23659c2f8fd62a38b1eef00a688
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-13 10:02:06 +0100

    Add Owl component with title and image display

---
+++ components/variableAttribute.js
@@ -0,0 +1,16 @@
+const owl = {
+  title: "Excellent Owl",
+  src: "https://content.codecademy.com/courses/React/react_photo-owl.jpg",
+};
+
+//Define function component here:
+function Owl() {
+  return (
+    <div>
+      <h1>{owl.title}</h1>
+      <img src = {owl.src} alt = {owl.title} />
+    </div>
+  );
+}
+
+export default Owl;
