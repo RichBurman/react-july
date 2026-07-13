95c36ae266374371e993ca95ed474acd69fa8b35
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-13 10:13:21 +0100

    Conditional Rendering

--- components/variableAttribute.js
+++ components/variableAttribute.js
@@ -45,3 +45,19 @@ function Friend() {
 }
 
 export default Friend;
+
+
+// Conditional Rendering
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New function component starts here:
+function TonightsPlan() {
+  if (fiftyFifty) {
+    return <h1>Tonight I'm going out WOOO</h1>
+  } else {
+    return <h1>Tonight I'm going to bed WOOO</h1>
+  }
+}
+
+export default TonightsPlan;
