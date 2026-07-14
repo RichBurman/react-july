e85f393630aa4dd065d91f79dbe66867049d573c
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-14 12:07:40 +0100

    Add task management components with state handling and rendering and a example of using the useState hook

---
+++ Hooks/example1/index.js
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom/client';
+import App from "./Container/AppFunction";
+
+createRoot(document.getElementById('app')).render(<App />);
---
+++ Hooks/useState.js
@@ -0,0 +1,15 @@
+// An great example of using the useState hook to toggle between two states.
+
+import { useState } from "react";
+
+function Toggle() {
+  const [toggle, setToggle] = useState();
+
+  return (
+    <div>
+      <p>The toggle is {toggle}</p>
+      <button onClick={() => setToggle("On")}>On</button>
+      <button onClick={() => setToggle("Off")}>Off</button>
+    </div>
+  );
+}
