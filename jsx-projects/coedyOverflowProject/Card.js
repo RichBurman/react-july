8064f302af58aa50d1fa2ad65c40a1957ae4273e
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-14 11:04:49 +0100

    Add code project about props

---
+++ jsx-projects/coedyOverflowProject/Card.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import Header from "./Header";
+
+function Card(props) {
+  return (
+    <div>
+      <Header profileImg={props.commentObject.profileImg} username={props.commentObject.username}/>
+      <Body comment={props.commentObject.comment}/>
+    </div>
+  );
+}
+
+export default Card;
